8c4519a16fe78cbe65fd64544114603b0eaddee4
KennyLi6 <kennyli3219@gmail.com>
2024-10-09 11:25:59 -0700

    added multiple autoincrement buttons that increase and cost differing amounts - step 6

--- src/main.ts
+++ src/main.ts
@@ -19,21 +19,54 @@ counter.innerHTML = count + " rats";
 button.addEventListener("click", function () {
     count++;
     counter.innerHTML = count + " rats";
-    growth_button.disabled = (count < 10);
 });
 app.append(counter);
 
-let growth_rate = 0;
-const growth_button = document.createElement("button");
-growth_button.innerHTML = "Create automatic rat"
-growth_button.addEventListener("click", function () {
-    growth_rate++;
-    count -= 10;
-    growth_counter.innerHTML = growth_rate + " rats per second"
+//thank you brace for helping formulate this increase_rps section of code
+type Command = () => void;
+
+function increase_rps(amount: number, cost: number): Command {
+  return () => {
+    growth_rate+=amount;
+    count -= cost;
+    growth_counter.innerHTML = growth_rate.toFixed(1) + " rats per second"
     requestAnimationFrame(countIncrement);
-});
-app.append(growth_button);
-growth_button.disabled = (count < 10);
+  }
+}
+
+const amount_A = 0.1;
+const amount_B = 2;
+const amount_C = 50;
+
+const cost_A = 10;
+const cost_B = 100;
+const cost_C = 1000;
+
+const increase_A: Command = increase_rps(amount_A, cost_A);
+const increase_B: Command = increase_rps(amount_B, cost_B);
+const increase_C: Command = increase_rps(amount_C, cost_C);
+
+// <br> is the \n of html, USE HTML SYNTAX WHEN TRYING TO DISPLAY TEXT
+const increase_A_text = "Increase rat production by " + amount_A + "<br> Cost: "+ cost_A;
+const increase_B_text = "Increase rat production by " + amount_B + "<br> Cost: "+ cost_B;
+const increase_C_text = "Increase rat production by " + amount_C + "<br> Cost: "+ cost_C;
+
+let growth_rate = 0;
+
+const growth_button_A = document.createElement("button");
+growth_button_A.innerHTML = increase_A_text;
+growth_button_A.addEventListener("click", increase_A);
+app.append(growth_button_A);
+
+const growth_button_B = document.createElement("button");
+growth_button_B.innerHTML = increase_B_text;
+growth_button_B.addEventListener("click", increase_B);
+app.append(growth_button_B);
+
+const growth_button_C = document.createElement("button");
+growth_button_C.innerHTML = increase_C_text;
+growth_button_C.addEventListener("click", increase_C);
+app.append(growth_button_C);
 
 const growth_counter = document.createElement("div");
 growth_counter.innerHTML = growth_rate + " rats per second"
@@ -41,18 +74,24 @@ app.append(growth_counter);
 
 let start_time: number | undefined;
 function countIncrement(timestamp: number) {
+  if (growth_rate > 0) {
     if (start_time === undefined) {
-        start_time = timestamp;
+      start_time = timestamp;
     }
     const elapsed = timestamp - start_time;
     const rat_per_second = elapsed / (1000 * (1 / growth_rate));
     counter.innerHTML = (count + rat_per_second).toFixed(2) + " rats";
         
     if (rat_per_second >= 1) {
-        count += 1;
-        counter.innerHTML = count + " rats";
-        start_time = timestamp;
+      count += 1;
+      counter.innerHTML = count + " rats";
+      start_time = timestamp;
     }
-    growth_button.disabled = (count < 10);
-    requestAnimationFrame(countIncrement);
+  }
+  growth_button_A.disabled = (count < 10);
+  growth_button_B.disabled = (count < 100);
+  growth_button_C.disabled = (count < 1000);
+  requestAnimationFrame(countIncrement);
 }
+
+requestAnimationFrame(countIncrement); //start clock when load
